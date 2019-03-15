56661e4057c17c5f2d14be4e16f7c2a9516889de
Josiah <Josiahsingh1996@gmail.com>
2019-03-15 16:17:09 +0200

    Validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://cloak:cloak1234@ds145555.mlab.com:45555/developerconnector'
-}
+    mongoURI: 'mongodb://cloak:cloak1234@ds145555.mlab.com:45555/developerconnector',
+    secretOrKey: 'secret'
+};
